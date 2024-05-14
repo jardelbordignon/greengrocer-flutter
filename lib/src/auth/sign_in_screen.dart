1b191ebaaf0b3322f48077f58537e8dcf2829433
Jardel Bordignon <jardel1101@gmail.com>
2024-05-14 20:30:15 -0300

    :sparkles: sign in: add sign-up button and remove expanded from form container

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -13,59 +13,102 @@ class SignInScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(child: Container(color: Colors.green)),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(25),
-                  )),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween, // space between
-                crossAxisAlignment:
-                    CrossAxisAlignment.stretch, // align-items stretch
-                children: [
-                  // Email
-                  const AppTextField(
-                    icon: Icons.email,
-                    label: 'Email',
-                  ),
-                  // Password
-                  const AppTextField(
-                    icon: Icons.lock,
-                    label: 'Password',
-                    isSecret: true,
-                  ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(25),
+                )),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween, // space between
+              crossAxisAlignment:
+                  CrossAxisAlignment.stretch, // align-items stretch
+              children: [
+                // Email
+                const AppTextField(
+                  icon: Icons.email,
+                  label: 'Email',
+                ),
+                // Password
+                const AppTextField(
+                  icon: Icons.lock,
+                  label: 'Password',
+                  isSecret: true,
+                ),
 
-                  SizedBox(
-                    height: 50,
-                    //width: 200,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
+                SizedBox(
+                  height: 50,
+                  //width: 200,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        onPressed: () {},
-                        child: const Text(
-                          'Sign In',
-                          style: TextStyle(fontSize: 18),
-                        )),
-                  ),
-
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
+                      ),
                       onPressed: () {},
                       child: const Text(
-                        'Esqueceu a senha?',
-                        style: TextStyle(color: Colors.redAccent),
+                        'Sign In',
+                        style: TextStyle(fontSize: 18),
+                      )),
+                ),
+
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(color: Colors.redAccent),
+                    ),
+                  ),
+                ),
+
+                // Divider
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey.withAlpha(90),
+                        thickness: 2,
                       ),
                     ),
-                  )
-                ],
-              ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10, right: 10, bottom: 12),
+                      child: Text('Ou'),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey.withAlpha(90),
+                        thickness: 2,
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Sign Up
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.green,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
