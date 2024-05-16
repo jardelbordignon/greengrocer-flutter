08ee6db59644b066956ac7f974ff92d3fbdcb651
Jardel Bordignon <jardel1101@gmail.com>
2024-05-16 08:52:13 -0300

    :sparkles: sign up: add form fields and submit button

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/components/app_text_field.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -34,6 +35,48 @@ class SignUpScreen extends StatelessWidget {
               borderRadius: BorderRadius.vertical(
                 top: Radius.circular(25),
               ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const AppTextField(
+                  icon: Icons.email,
+                  label: 'Email',
+                ),
+                const AppTextField(
+                  icon: Icons.lock,
+                  label: 'Password',
+                  isSecret: true,
+                ),
+                const AppTextField(
+                  icon: Icons.person,
+                  label: 'Name',
+                ),
+                const AppTextField(
+                  icon: Icons.phone,
+                  label: 'Phone',
+                ),
+                const AppTextField(
+                  icon: Icons.file_copy,
+                  label: 'Document Number',
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      backgroundColor: Colors.green,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign Up Now',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
             ),
           )
         ],
