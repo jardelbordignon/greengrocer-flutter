9d20a2cc66919e53cc121895d50a0f21db1f4b37
Jardel Bordignon <jardel1101@gmail.com>
2024-05-15 21:38:17 -0300

    :sparkles: sign in: wrap screen with ScrollView

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -12,170 +12,180 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // App name
-                Text.rich(
-                  TextSpan(
-                    style: const TextStyle(
-                      fontSize: 40,
-                    ),
-                    children: [
-                      const TextSpan(
-                        text: 'Green',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'grocer',
-                        style: TextStyle(
-                          color: CustomColors.customContrastColor,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                // Categories
-                SizedBox(
-                  height: 30,
-                  child: DefaultTextStyle(
-                    style: const TextStyle(fontSize: 25),
-                    child: AnimatedTextKit(
-                      repeatForever: true,
-                      pause: Duration.zero,
-                      animatedTexts: [
-                        FadeAnimatedText('Frutas'),
-                        FadeAnimatedText('Verduras'),
-                        FadeAnimatedText('Legumes'),
-                        FadeAnimatedText('Carnes'),
-                        FadeAnimatedText('Cereais'),
-                        FadeAnimatedText('Laticínios'),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          // Formulary
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(25),
-              ),
-            ),
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween, // space between
-              crossAxisAlignment:
-                  CrossAxisAlignment.stretch, // align-items stretch
-              children: [
-                // Email
-                const AppTextField(
-                  icon: Icons.email,
-                  label: 'Email',
-                ),
-                // Password
-                const AppTextField(
-                  icon: Icons.lock,
-                  label: 'Password',
-                  isSecret: true,
-                ),
-
-                SizedBox(
-                  height: 50,
-                  //width: 200,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      backgroundColor: Colors.green,
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Sign In',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Esqueceu a senha?',
-                      style: TextStyle(
-                        color: CustomColors.customContrastColor,
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Divider
-                Row(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(
-                      child: Divider(
-                        color: Colors.grey.withAlpha(90),
-                        thickness: 2,
+                    // App name
+                    Text.rich(
+                      TextSpan(
+                        style: const TextStyle(
+                          fontSize: 40,
+                        ),
+                        children: [
+                          const TextSpan(
+                            text: 'Green',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'grocer',
+                            style: TextStyle(
+                              color: CustomColors.customContrastColor,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 10, right: 10, bottom: 12),
-                      child: Text('Ou'),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        color: Colors.grey.withAlpha(90),
-                        thickness: 2,
+
+                    // Categories
+                    SizedBox(
+                      height: 30,
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 25),
+                        child: AnimatedTextKit(
+                          repeatForever: true,
+                          pause: Duration.zero,
+                          animatedTexts: [
+                            FadeAnimatedText('Frutas'),
+                            FadeAnimatedText('Verduras'),
+                            FadeAnimatedText('Legumes'),
+                            FadeAnimatedText('Carnes'),
+                            FadeAnimatedText('Cereais'),
+                            FadeAnimatedText('Laticínios'),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
+              ),
 
-                // Sign Up
-                SizedBox(
-                  height: 50,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      side: const BorderSide(
-                        width: 2,
-                        color: Colors.green,
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
+              // Formulary
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(25),
                   ),
                 ),
-              ],
-            ),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween, // space between
+                  crossAxisAlignment:
+                      CrossAxisAlignment.stretch, // align-items stretch
+                  children: [
+                    // Email
+                    const AppTextField(
+                      icon: Icons.email,
+                      label: 'Email',
+                    ),
+                    // Password
+                    const AppTextField(
+                      icon: Icons.lock,
+                      label: 'Password',
+                      isSecret: true,
+                    ),
+
+                    SizedBox(
+                      height: 50,
+                      //width: 200,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          backgroundColor: Colors.green,
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(
+                            color: CustomColors.customContrastColor,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // Divider
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
+                          ),
+                        ),
+                        const Padding(
+                          padding:
+                              EdgeInsets.only(left: 10, right: 10, bottom: 12),
+                          child: Text('Ou'),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    // Sign Up
+                    SizedBox(
+                      height: 50,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          side: const BorderSide(
+                            width: 2,
+                            color: Colors.green,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
