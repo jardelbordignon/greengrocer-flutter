4e3377cfc5a84e0700f0f805e423f413a992d482
Jardel Bordignon <jardel1101@gmail.com>
2024-05-20 13:54:46 -0300

    :sparkles: products item_tile format price and add_button

--- lib/src/screens/home/components/item_tile.dart
+++ lib/src/screens/home/components/item_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
   final ItemModel item;
@@ -8,63 +10,91 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      // child: const ListTile(
-      //   title: Text('Item'),
-      //   subtitle: Text('Description'),
-      //   trailing: Icon(Icons.add),
-      // ),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Image
-            Expanded(
-              child: Image.asset(item.imageUrl),
-            ),
-
-            // Product name
-            Text(
-              item.name,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            // Price and unit
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 3,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          // child: const ListTile(
+          //   title: Text('Item'),
+          //   subtitle: Text('Description'),
+          //   trailing: Icon(Icons.add),
+          // ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                // Price
+                // Image
+                Expanded(
+                  child: Image.asset(item.imageUrl),
+                ),
+
+                // Product name
                 Text(
-                  'R\$ ${item.price}',
+                  item.name,
                   style: const TextStyle(
-                    fontSize: 20,
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    color: Colors.green,
                   ),
                 ),
 
-                // Unit
-                Text(
-                  ' /${item.unit}',
-                  style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                  ),
+                // Price and unit
+                Row(
+                  children: [
+                    // Price
+                    Text(
+                      utilsServices.priceToCurrency(item.price),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+
+                    // Unit
+                    Text(
+                      ' /${item.unit}',
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+
+        // Add to cart button
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                color: CustomColors.customSwatchColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+              ),
+              height: 40,
+              width: 35,
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
