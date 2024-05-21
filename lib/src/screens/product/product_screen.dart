bd6245068ce792b81789fae0e7c0f441ed34b817
Jardel Bordignon <jardel1101@gmail.com>
2024-05-20 21:45:09 -0300

    :sparkles: product details: content data

--- lib/src/screens/product/product_screen.dart
+++ lib/src/screens/product/product_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
   final ItemModel item;
@@ -10,31 +13,91 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withAlpha(230),
-      body: Column(children: [
-        // Image
-        Expanded(
-          child: Image.asset(item.imageUrl),
-        ),
-
-        // Content
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(40),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.shade600,
-                  offset: const Offset(0, 2),
-                  blurRadius: 3,
-                ),
-              ],
-            ),
+      body: Column(
+        children: [
+          // Image
+          Expanded(
+            child: Image.asset(item.imageUrl),
           ),
-        )
-      ]),
+
+          // Content
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(40),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade600,
+                    offset: const Offset(0, 2),
+                    blurRadius: 3,
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // name - quantity
+                  Row(
+                    children: [
+                      // name
+                      Expanded(
+                        child: Text(
+                          item.name,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      // quantity
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+
+                  // price
+                  Text(
+                    utilsServices.priceToCurrency(item.price),
+                    style: const TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+
+                  // description
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          item.description,
+                          style: const TextStyle(
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Add to cart button
+                  ElevatedButton(onPressed: () {}, child: const Text('Submit'))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
