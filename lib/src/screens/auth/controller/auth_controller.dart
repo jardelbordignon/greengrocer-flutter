27daff9d35abe53cc2f8f03f494a7cf32b5c2df6
Jardel Bordignon <jardel1101@gmail.com>
2024-06-06 14:00:53 -0300

    :sparkles: authentication request by auth_controller

--- lib/src/screens/auth/controller/auth_controller.dart
+++ lib/src/screens/auth/controller/auth_controller.dart
@@ -1,14 +1,17 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/screens/auth/repository/auth_repository.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
+
+  final authRepository = AuthRepository();
 
   Future<bool> signIn({
     required String email,
     required String password,
   }) async {
     isLoading.value = true;
-    await Future.delayed(const Duration(seconds: 3));
+    await authRepository.signIn(email: email, password: password);
     isLoading.value = false;
 
     if (password == 'Pwd@123!') {
