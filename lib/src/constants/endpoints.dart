3f015ab5f99c8b87d3b14fed1e592cd7a4ee4919
Jardel Bordignon <jardel1101@gmail.com>
2024-06-06 12:10:03 -0300

    :sparkles: auth repository

---
+++ lib/src/constants/endpoints.dart
@@ -0,0 +1,4 @@
+abstract class Endpoints {
+  static const String signIn = '/login';
+  static const String signUp = '/signup';
+}
---
+++ lib/src/screens/auth/repository/auth_repository.dart
@@ -0,0 +1,18 @@
+import 'package:greengrocer/src/constants/endpoints.dart';
+import 'package:greengrocer/src/services/http_manager.dart';
+
+class AuthRepository {
+  Future signIn({required String email, required String password}) async {
+    httpManager.request(
+      url: Endpoints.signIn,
+      method: HttpMethods.post,
+      body: { 'email': email, 'password': password },
+      onError: (error) {
+        print(error);
+      },
+      onSuccess: (data) {
+        print(data);
+      },
+    );
+  }
+}
